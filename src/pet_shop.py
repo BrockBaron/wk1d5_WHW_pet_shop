b3d440767d80be52dea42261318d34a1938c2c1a
Nick Lekkas <nslekkas@gmail.com>
2021-03-12 14:57:36 +0000

    update 1

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,10 +1,16 @@
-# WRITE YOUR FUNCTIONS HERE
-
+#1
 def get_pet_shop_name(pet_shop):
-    return pet_shop['name'] #[]refering to a dictionary key, either {} or []
+    return pet_shop['name']             #[]refering to a dictionary key, either {} or []
     print([])
     
 
-
+#2
 def get_total_cash(pet_shop):
     return pet_shop['admin']['total_cash']
+
+#3
+def add_or_remove_cash(pet_shop, amount):
+    pet_shop['admin']['total_cash'] += amount
+#    get_total_cash(pet_shop)+= amount
+
+#4
