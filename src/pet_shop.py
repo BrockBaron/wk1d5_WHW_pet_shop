507d1f1453abbb72e07bc23ae040bb2c253d5803
Nick Lekkas <nslekkas@gmail.com>
2021-03-14 22:12:05 +0000

    Options 1/2 completes

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -5,7 +5,6 @@ from typing import Collection, Counter
 def get_pet_shop_name(pet_shop):
     return pet_shop['name']             #[]refering to a dictionary key, either {} or []
     print([])
-    
 
 #2
 def get_total_cash(pet_shop):
@@ -70,8 +69,50 @@ def remove_customer_cash(customer, money):
 #16
 def get_customer_pet_count(customer):
     return len(customer['pets'])
-    
 
 #17    
 def add_pet_to_customer(customer, new_pet):
     return customer['pets'].append(new_pet)    
+
+#18 & 19 & 20
+def customer_can_afford_pet(customer, new_pet):
+    if customer['cash']>= new_pet['price']:
+        return True
+    elif customer['cash']< new_pet['price']:
+        return False
+    
+
+#21
+
+def sell_pet_to_customer(pet_shop, pet, customer):
+    customer['pets']+= pet
+    pet_shop['admin']['pets_sold'] += 1
+    
+    get_customer_pet_count.append() # add or subtract pet cound dependant on if selling or buying from customer
+    
+    get_pets_sold.append() # inrease pet sold in sale or state 0 in buy
+    
+    get_customer_cash.append() # subtract in purchase of pet, increase in sale of pet
+    
+    get_total_cash.apped() # increase in sale of pet, decrease in purchase of pet
+    
+    
+# add pet to customer pets list increase value by 1 - retrun get customer pet count funciton
+
+# increase value 'number' in pets sold - return get pets sold fucntion
+
+# change customers cash after purchase i.e. get customer remaining cash after purchase - return get customer cash
+
+# increase value of total cash in shop in crease value by 900 - return get total cash in shop
+
+
+
+# reset values of pets sold and customer owned values
+
+# create loop (for or while) to take customer cash and add it to total cash
+
+# create loop to remove pet from shop and add it to customer pet value count
+
+#create logic or loop to either sell or buy , changing the dirctiong of customer cash and shop cash, and customer pet count and shops pet count.
+
+
